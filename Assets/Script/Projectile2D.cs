d30fa2a1094521ba53f773a073e608271670c65e
puauksonboon-lang <puaukson.boon@bumail.net>
2026-05-01 22:29:07 +0700

    Shooter

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -1,27 +1,20 @@
 using UnityEngine;
-using UnityEngine.InputSystem; // ใช้ Input แบบใหม่โดยไม่ต้องไปตั้งค่า
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField] private Transform shootPoint;     // จุดที่ยิงออก
-    [SerializeField] private GameObject target;        // เป้าเล็ง / Crosshair
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject target;
     [SerializeField] private Rigidbody2D bulletPrefab;
-
-    void Start()
-    {
-    }
 
     void Update()
     {
-        Vector2 screenPos = Mouse.current.position.ReadValue(); // อ่านตำแหน่งเมาส์
+        Vector2 screenPos = Input.mousePosition;
 
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (Input.GetMouseButtonDown(0))
         {
-            // ยิง Ray ไปตามตำแหน่งเมาส์
             Ray ray = Camera.main.ScreenPointToRay(screenPos);
             Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
 
-            // ตรวจจับ Ray ชนอะไร
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
 
             if (hit.collider != null)
@@ -29,21 +22,18 @@ public class Shooter : MonoBehaviour
                 target.transform.position = new Vector2(hit.point.x, hit.point.y);
                 Debug.Log($"Hit {hit.collider.gameObject.name}");
 
-                // คำนวณความเร็วกระสุน
                 Vector2 projectileVelocity = CalculateProjectileVelocity(
                     shootPoint.position,
                     hit.point,
                     1f
                 );
 
-                // สร้างกระสุน
                 Rigidbody2D shootBullet = Instantiate(
                     bulletPrefab,
                     shootPoint.position,
                     Quaternion.identity
                 );
 
-                // ใส่ความเร็วให้กระสุน
                 shootBullet.velocity = projectileVelocity;
             }
         }
@@ -51,7 +41,6 @@ public class Shooter : MonoBehaviour
 
     Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
     {
-        // time = เวลาในการบิน
         Vector2 direction = target - origin;
 
         return new Vector2(
